98d9939beafecaf77314a7ceb1c04d767dec1f38
Philip <philip@fuzzy.work>
2020-02-19 11:34:04 +0000

    - Added database-first entities and context. - Added controller. - Added list service. - Modified URL in http client. - Removed pesky IIS Express profile from both projects.

---
+++ BlazorDataService/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorDataService.Models
+{
+    public partial class ToDo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
--- BlazorDataService/Startup.cs
+++ BlazorDataService/Startup.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using BlazorDataService.Models;
+using BlazorDataService.Services;
 
 namespace BlazorDataService
 {
@@ -25,6 +30,12 @@ namespace BlazorDataService
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices (IServiceCollection services)
 		{
+			services.AddDbContext<ApplicationDbContext> (options =>
+				 options.UseSqlServer (
+					 Configuration.GetConnectionString ("DefaultConnection")));
+
+			services.AddTransient<IToDoListService, ToDoListService> ();
+
 			services.AddControllers ();
 		}
 
